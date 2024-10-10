6c89e74ff551bee19764af6338622511032d8ac5
dalhatmd <umarwalid4055@gmail.com>
2024-10-10 09:34:00 +0000

    Add task 8

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -4,6 +4,8 @@ from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """ multiplies a number by itself """
     def multiply(x: float) -> float:
+        """ returns multuplier * x """
         return multiplier * x
     return multiply
