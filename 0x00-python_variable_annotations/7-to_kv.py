906ace79853421daba4a5bdfc83e437ee853cafe
dalhatmd <umarwalid4055@gmail.com>
2024-10-10 13:09:27 +0000

    Fix task 7

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -6,4 +6,4 @@ from typing import Union
 
 def to_kv(k: str, v: Union[int | float]) -> Tuple[str, float]:
     """ returns k and v squared """
-    return f"('{k}', {v ** 2})"
+    return f"('{k}', {float(v * v)})"
