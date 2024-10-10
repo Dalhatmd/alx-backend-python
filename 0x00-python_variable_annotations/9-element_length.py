77e7230d7bb4f304a95c37223cb8b748114fbd57
dalhatmd <umarwalid4055@gmail.com>
2024-10-10 13:02:40 +0000

    Fix task 9

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,8 +1,9 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """ annotate function """
 from typing import List, Sequence, Tuple, Iterable
 
 
-def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+def element_length(lst: Iterable[Sequence]) -> \
+                   List[Tuple[Sequence, int]]:
     """ returns a tuple """
     return [(i, len(i)) for i in lst]
