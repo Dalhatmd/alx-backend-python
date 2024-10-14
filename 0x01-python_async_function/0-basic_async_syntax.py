67eef263a13a0665fed655d2bdcd702d04f758a1
dalhatmd <umarwalid4055@gmail.com>
2024-10-14 11:42:41 +0000

    Add type annotation

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -4,7 +4,7 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay: int = 10) -> float:
     """ waits for random amount of seconds """
     number = random.random() * max_delay
     await asyncio.sleep(number)
