e168e36cacacd20a04e56d48834c541ff056ab05
dalhatmd <umarwalid4055@gmail.com>
2024-10-14 12:44:26 +0000

    Fix task 1 and 2

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 """ task 1 module """
-
-
 import asyncio
 from typing import List
+
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 """ measures the runtime """
-
-
 import asyncio
 import time
+
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
