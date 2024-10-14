69060d424912457f538e379c9172dce5fb414fe1
dalhatmd <umarwalid4055@gmail.com>
2024-10-14 11:47:17 +0000

    Fix formatting

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -8,9 +8,7 @@ async def wait_n(n: int, max_delay: int) -> list:
     """ waits for {max_delay} seconds {n} times """
     tasks = [wait_random(max_delay) for _ in range(n)]
     delays = []
-
     for task in asyncio.as_completed(tasks):
         result = await task
         delays.append(result)
-
     return delays
