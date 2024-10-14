aa8f9e58676589bd6c47a1cd50c5386dfd3e46e4
dalhatmd <umarwalid4055@gmail.com>
2024-10-14 19:11:25 +0000

    change algorithm

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -7,11 +7,15 @@ import random
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
-    """ waits for {max_delay} seconds {n} times """
-    tasks = [wait_random(max_delay) for _ in range(n)]
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """waits for max_delay seconds """
     delays = []
-    for task in asyncio.as_completed(tasks):
-        result = await task
-        delays.append(result)
+
+    for _ in range(n):
+        delay = await wait_random(max_delay)
+        i = 0
+        while i < len(delays) and delays[i] < delay:
+            i += 1
+        delays.insert(i, delay)
+
     return delays
