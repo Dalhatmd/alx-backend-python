3c214ac0d2375ed721ab6267e9c48c2f83b3dd50
dalhatmd <umarwalid4055@gmail.com>
2024-10-14 19:48:11 +0000

    Fix task 4

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -6,11 +6,8 @@ from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> list[float]:
-    """ waits for {max_delay} seconds {n} times """
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """ like wait_n but task_wait_random is called instead"""
     tasks = [task_wait_random(max_delay) for _ in range(n)]
-    delays = []
-    for task in asyncio.as_completed(tasks):
-        result = await task
-        delays.append(result)
-    return delays
+    delays = await asyncio.gather(*tasks)
+    return sorted(delays)
