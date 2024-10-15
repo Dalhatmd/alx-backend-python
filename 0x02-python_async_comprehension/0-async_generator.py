1e38514c3b6829abe16c801f40935346e2f14c8b
dalhatmd <umarwalid4055@gmail.com>
2024-10-15 15:28:44 +0000

    Fix type annotation

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -3,9 +3,10 @@
 import asyncio
 import random
 import time
+from typing import AsyncGenerator
 
 
-async def async_generator():
+async def async_generator() -> AsyncGenerator[float, None]:
     """ yields a random number 10 times """
     for _ in range(10):
         yield random.random() * 10
