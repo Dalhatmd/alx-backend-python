fe4d7219e758de04a10a7942760e2be18376d191
dalhatmd <umarwalid4055@gmail.com>
2024-11-04 10:06:11 +0100

    Add test for public repos method

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,7 +2,7 @@
 """ Test module for Client module """
 import unittest
 from client import GithubOrgClient
-from unittest.mock import Mock, patch, MagicMock
+from unittest.mock import Mock, patch, MagicMock, PropertyMock
 from parameterized import parameterized
 from utils import get_json
 
@@ -11,7 +11,7 @@ class TestGithubOrgClient(unittest.TestCase):
     """ GitHubOrgClient Test suite """
     @parameterized.expand([("google",), ("abc",)])
     @patch('utils.requests.get')
-    def test_org(self, org_name, mock_get_json):
+    def test_org(self, org_name: str, mock_get_json: MagicMock):
         """ tests for correct org return """
         mock_response = MagicMock()
         mock_response.json.return_value = {"login": "test_org",
@@ -26,6 +26,17 @@ class TestGithubOrgClient(unittest.TestCase):
 
         mock_get_json.assert_called_once_with(expected_url)
         self.assertEqual(result, mock_response.json.return_value)
+
+    @parameterized.expand([("google",), ("abc",)])
+    @patch.object(GithubOrgClient,
+                  "_public_repos_url",
+                  new_callable=PropertyMock)
+    def test_public_repos_url(self, org_name, mock_public_repos_url):
+        mock_public_repos_url.return_value = "https://api.github.com/orgs\
+                /test_org/repos"
+        client = GithubOrgClient(org_name)
+        repos_url = client._public_repos_url
+        self.assertEqual(repos_url, mock_public_repos_url.return_value)
 
 
 if __name__ == "__main__":
